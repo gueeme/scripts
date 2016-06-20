3b5e8c8fd86d5b43fa741df50fb53bb5afc55974
gueeme <guilherme.it@me.com>
2016-06-20 11:49:00 -0300

    Add files via upload

---
+++ BKP-GPO.ps1
@@ -0,0 +1,20 @@
+﻿$data = Get-Date -Format dd.MM.yyyy
+$nome = "$data"
+New-Item -Path "\\BACKUP-SERVER\GPOs\$data" -ItemType directory
+New-Item -Path "C:\LOG\GPOS\$data" -ItemType directory
+
+Backup-GPO -Path "\\BACKUP-SERVER\GPOs\$data" -All -Server SERVERNAME.domain.local | Out-File C:\LOG\GPOS\$data\log-gpo.txt
+
+$filename = “C:\LOG\GPOS\$data\log-gpo.txt”
+$smtpServer = “smtpserver.com.br”
+$msg = new-object Net.Mail.MailMessage
+$att = new-object Net.Mail.Attachment($filename)
+$smtp = new-object Net.Mail.SmtpClient($smtpServer)
+$smtp.Credentials = New-Object Net.NetworkCredential(“exemplo@exemplo.com”, “SENHA”)
+$msg.From = “exemplo@exemplo.com”
+$msg.To.Add(“exemplo@exemplo.com”)
+$date = get-date
+$msg.Subject = “BACKUP GPOS – $date”
+$msg.Body = “BACKUP DE GPOS CONCLUÍDO. VERIFIQUE O LOG EM ANEXO PARA CERTIFICAR QUE NÃO HOUVE ERRO.”
+$msg.Attachments.Add($att)
+$smtp.Send($msg)
---
+++ BKP-SYSTEMSTATE [MODELO].ps1
@@ -0,0 +1,20 @@
+﻿$date = Get-Date -Format dd.MM.yyyy 
+
+New-Item -Path "\\BACKUP-SERVER\SYSTEMSTATE\$data" -ItemType directory
+New-Item -Path "C:\LOG\SYSTEMSTATE\$data" -ItemType directory
+
+wbadmin start systemstatebackup -backupTarget:$backdir | Out-File C:\LOG\SYSTEMSTATE\$data\log-systemstate.txt
+
+$filename = “C:\LOG\SYSTEMSTATE\$data\log-systemstate.txt”
+$smtpServer = “smtpserver.com.br”
+$msg = new-object Net.Mail.MailMessage
+$att = new-object Net.Mail.Attachment($filename)
+$smtp = new-object Net.Mail.SmtpClient($smtpServer)
+$smtp.Credentials = New-Object Net.NetworkCredential(“exemplo@exemplo.com”, “SENHA”)
+$msg.From = “exemplo@exemplo.comr”
+$msg.To.Add(“exemplo@exemplo.com”)
+$date = get-date
+$msg.Subject = “BACKUP GPOS – $date”
+$msg.Body = “BACKUP DA SYSTEMSTATE CONCLUÍDO. VERIFIQUE O LOG EM ANEXO PARA CERTIFICAR QUE NÃO HOUVE ERRO.”
+$msg.Attachments.Add($att)
+$smtp.Send($msg)
